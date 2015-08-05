ab19af028cfbbc8578d8837618abcbc4c3ef79b4
hanpreet Kaur <>
2015-08-05 13:57:52 -0400

    set and get methods to post the feedback

---
+++ src/java/Demo/feedbackprop.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Demo;
+
+/**
+ *
+ * @author hanpreet
+ */
+public class feedbackprop {
+     private String fname,lname,email,mobile,enq;
+    private int eid;
+     public void setFname(String f)
+    {
+        fname=f;
+    }
+     public String getFname()
+    {
+        return fname;
+    }
+      public void setLname(String l)
+    {
+        lname=l;
+    }
+     public String getLname()
+    {
+        return lname;
+    }
+       public void setEmail(String e)
+    {
+        email=e;
+    }
+       public String getEmail()
+    {
+        return email;
+    }
+       public void setEnq(String l)
+    {
+        enq=l;
+    }
+       public String getEnq()
+    {
+        return enq;
+    }
+       public void setMob(String m)
+    {
+        mobile=m;
+    }
+        public String getMob()
+    {
+        return mobile;
+    }
+     public void setEid(int m)
+    {
+        eid=m;
+    }
+        public int getEid()
+    {
+        return eid;
+    }    
+        
+}
