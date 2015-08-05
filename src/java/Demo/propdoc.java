5e2242b41086322b83f120c588a813e3cf1c2892
hanpreet Kaur <>
2015-08-05 14:02:14 -0400

    sql dependency in jsp pages

---
+++ src/java/Demo/propdoc.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Demo;
+
+/**
+ *
+ * @author hanpreet
+ */
+public class propdoc {
+    private String dname,dob,gen,qual,email,contact,add,login,pwd,hid,spec,subspec,img,pid,rep;
+    int did;
+      public void setdName(String n)
+    {
+        dname=n;
+    }
+     public String getdName()
+    {
+        return dname;
+    }
+     public void setPid(String n)
+    {
+        pid=n;
+    }
+     public String getPid()
+    {
+        return pid;
+    }
+      public void setRep(String n)
+    {
+        rep=n;
+    }
+     public String getRep()
+    {
+        return rep;
+    }
+     public void setAge(String n)
+    {
+        dob=n;
+    }
+     public String getAge()
+    {
+        return dob;
+    }
+      public void setGen(String n)
+    {
+        gen=n;
+    }
+     public String getGen()
+    {
+        return gen;
+    }
+      public void setQual(String n)
+    {
+        qual=n;
+    }
+     public String getQual()
+    {
+        return qual;
+    }
+      public void setSpec(String n)
+    {
+        spec=n;
+    }
+     public String getSpec()
+    {
+        return spec;
+    }
+      public void setSub(String n)
+    {
+        subspec=n;
+    }
+     public String getSub()
+    {
+        return subspec;
+    }
+      public void setmail(String n)
+    {
+        email=n;
+    }
+     public String getmail()
+    {
+        return email;
+    }
+      public void setNum(String n)
+    {
+        contact=n;
+    }
+     public String getNum()
+    {
+        return contact;
+    }
+      public void setAdd(String n)
+    {
+        add=n;
+    }
+     public String getAdd()
+    {
+        return add;
+    }
+      public void setHid(String n)
+    {
+        hid=n;
+    }
+     public String getHid()
+    {
+        return hid;
+    }
+      public void setLogin(String n)
+    {
+        login=n;
+    }
+     public String getLogin()
+    {
+        return login;
+    }
+      public void setPwd(String n)
+    {
+        pwd=n;
+    }
+     public String getPwd()
+    {
+        return pwd;
+    }
+       public void setImg(String n)
+    {
+        img=n;
+    }
+     public String getImg()
+    {
+        return img;
+    }
+     public void setId(int n)
+    {
+        did=n;
+    }
+     public int getId()
+    {
+        return did;
+    }
+}
