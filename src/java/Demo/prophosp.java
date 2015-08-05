4ec6cb7be19afb1bd972116ebdfe3714ef6c6cfc
hanpreet Kaur <>
2015-08-05 13:52:52 -0400

    Class to search the hospital

---
+++ src/java/Demo/prophosp.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Demo;
+
+/**
+ *
+ * @author  hanpreet
+ */
+public class prophosp {
+    private String hname,add,city,state,pin,email,contact,web;
+      public void sethName(String n)
+    {
+        hname=n;
+    }
+     public String gethName()
+    {
+        return hname;
+    }
+      public void setAdd(String n)
+    {
+        add=n;
+    }
+     public String getAdd()
+    {
+        return add;
+    }
+      public void setCity(String n)
+    {
+        city=n;
+    }
+     public String getCity()
+    {
+        return city;
+    }
+      public void setState(String n)
+    {
+        state=n;
+    }
+     public String getState()
+    {
+        return state;
+    }
+      public void setPin(String n)
+    {
+        pin=n;
+    }
+     public String getPin()
+    {
+        return pin;
+    }
+      public void setmail(String n)
+    {
+        email=n;
+    }
+     public String getmail()
+    {
+        return email;
+    }
+      public void setNum(String n)
+    {
+        contact=n;
+    }
+     public String getNum()
+    {
+        return contact;
+    }
+      public void setWeb(String n)
+    {
+        web=n;
+    }
+     public String getWeb()
+    {
+        return web;
+    }
+     
+      
+}
